881fd2d060d1830e81516173395f0b761806e6a0
Prince <princedhameliya43@gmail.com>
2024-06-18 11:58:26 +0530

    commit changes

--- src/Components/News.js
+++ src/Components/News.js
@@ -14,7 +14,7 @@ export class News extends Component {
             "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
             "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det…"
         },
         {
             "source": {
@@ -27,7 +27,7 @@ export class News extends Component {
             "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
             "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
             "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried…"
         }
     ]
     constructor() {
@@ -35,7 +35,47 @@ export class News extends Component {
         console.log("Hello! I am a constructor from NewsComponent")
         this.state = {
             articles: this.articles,
-            loading: false
+            loading: false,
+            page: 1,
+            totalResults: 0
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=054ba42c7a8e45e98faea294ed081755&page=1&pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+    }
+
+    handlePrevClick = async () => {
+        console.log("Previous page loaded...")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=054ba42c7a8e45e98faea294ed081755&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+
+        else {
+            console.log("Next page loaded...")
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=054ba42c7a8e45e98faea294ed081755&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
         }
     }
 
@@ -45,16 +85,17 @@ export class News extends Component {
                 <div className='container my-3'>
                     <h2>NewsMonkey - Top Headlines</h2>
                     <div className='row'>
-                        <div className='col-md-4'>
-                            <NewsItems title="myTitle" description="myDescription"
-                                imageUrl="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" />
-                        </div>
-                        <div className='col-md-4'>
-                            <NewsItems title="myTitle" description="myDescription" />
-                        </div>
-                        <div className='col-md-4'>
-                            <NewsItems title="myTitle" description="myDescription" />
-                        </div>
+                        {Array.isArray(this.state.articles) && this.state.articles.map((element) => {
+                            return <div className='col-md-4' key={element.url}>
+                                <NewsItems title={element.title ? element.title.slice(0, 45) : ""}
+                                    description={element.description ? element.description.slice(0, 88) : ""}
+                                    imageUrl={element.urlToImage} newsUrl={element.url} />
+                            </div>
+                        })}
+                    </div>
+                    <div className='container d-flex justify-content-between'>
+                        <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                     </div>
                 </div>
             </>
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -7,16 +7,16 @@ export class NewsItems extends Component {
     }
 
     render() {
-        let { title, description, imageUrl } = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
             <>
                 <div className='my-3'>
                     <div className="card" style={{ width: "18rem" }}>
-                        <img src={imageUrl} className="card-img-top" alt="..." />
+                        <img src={!imageUrl?"https://i.timesnowhindi.com/stories/Virat-Kohli-Cover-Drive.jpg":imageUrl} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsUrl} target='__blank' className="btn btn-sm btn-dark">Read More</a>
                         </div>
                     </div>
                 </div>
